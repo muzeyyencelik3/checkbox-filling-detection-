1c1c42e486f7cd0f50abd47f9a3b41edb3e35c88
muzeyyencelik3 <muzeyye.celik2@ogr.sakarya.edu.tr>
2023-11-16 02:01:27 +0300

    Apply thresholding and contour detection for white regions in camera feed.

--- main.cpp
+++ main.cpp
@@ -35,9 +35,20 @@ int main() {
           // Convert the frame to grayscale
         cv::Mat gray;
         cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        
+        // Apply thresholding to detect white regions (adjust threshold value as needed)
+        cv::Mat whiteMask;
+        cv::threshold(gray, whiteMask, 170, 200, cv::THRESH_BINARY);
 
-      
+        // Find contours in the binary image
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(whiteMask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
+        // Draw contours on the original frame
+        cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0), 2);
+
+      // Display the frame in the window
+        cv::imshow("Camera Feed", frame);
 
 
         // Check if ESC key is pressed to exit the loop
