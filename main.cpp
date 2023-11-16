22d7c3073ba90cb431e9aa536a3d48a5b75ec614
muzeyyencelik3 <muzeyye.celik2@ogr.sakarya.edu.tr>
2023-11-16 11:02:03 +0300

    In this commit, added functionality to match each checkbox contour to a specific question based on its position or other criteria. The example assumes a simple strategy where checkboxes on the left belong to question 1, and checkboxes on the right belong to question 2. The program draws rectangles around identified checkboxes and prints their centroid coordinates.

--- main.cpp
+++ main.cpp
@@ -45,7 +45,36 @@ int main() {
         cv::findContours(whiteMask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
         // Draw contours on the original frame
-        cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0), 2);
+        //*************cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0), 2);
+        
+        
+        for (const auto& contour : contours) {
+            double area = cv::contourArea(contour);
+            if (area > 100) {
+                // Calculate centroid of the contour
+                cv::Moments moments = cv::moments(contour);
+                cv::Point centroid(moments.m10 / moments.m00, moments.m01 / moments.m00);
+
+                // TODO: Match each checkbox contour to a specific question
+                // For simplicity, this example draws rectangles around the identified checkboxes
+                cv::Rect boundingBox = cv::boundingRect(contour);
+                cv::rectangle(frame, boundingBox, cv::Scalar(0, 255, 0), 2);
+
+                // You can use the centroid position or other criteria to determine the question
+                // Here, it simply prints the centroid coordinates as an example
+                
+                
+                // *****In this simple example, we assume that checkboxes on the left belong to question 1,
+                // and checkboxes on the right belong to question 2.
+                // You may need a more sophisticated approach or a machine learning model in a real scenario.
+                
+                 int questionNumber = (centroid.x < frame.cols / 2) ? 1 : 2;
+
+                cv::Rect boundingBox = cv::boundingRect(contour);
+                cv::rectangle(frame, boundingBox, cv::Scalar(0, 255, 0), 2);
+                std::cout << "Checkbox at (" << centroid.x << ", " << centroid.y << ") belongs to a question." << std::endl;
+            }
+        }   //Deneme
 
       // Display the frame in the window
         cv::imshow("Camera Feed", frame);
