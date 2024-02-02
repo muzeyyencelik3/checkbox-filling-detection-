3104f8aa4b17c0601f2a1a5bd3fd93502aa09333
müzeyyen çelik <muzeyyen.celik2@ogr.sakarya.edu.tr>
2024-02-02 12:18:43 +0300

    update

--- checkbox.cpp
+++ checkbox.cpp
@@ -1,81 +1,127 @@
 #include <opencv2/opencv.hpp>
-#include <iostream>
-#include <vector>
 
 using namespace cv;
 
 int main() {
-    
-   
+    // Resmi yükle
+    Mat image = imread("/home/muzeyyen/Masaüstü/CheckBox/sample3.png");
+    if (image.empty()) {
+        std::cerr << "Resim yüklenemedi." << std::endl;
+        return -1;
+    }
 
-	// Fotoğrafın bulunduğu dizin ve adı
-	std::string path = "/home/kali/Documents/image/images/photo.jpg";
-
-	// Fotoğrafı okuyup detectedA4 değişkenine atıyoruz
-	cv::Mat detectedA4 = cv::imread(path);
-
-	// Gri tonlamalı görüntü oluşturma (gerekli olabilir ve bundan sonraki bikaç satır sonra silinecek)
+    // Gri tonlamaya dönüştür
     Mat gray;
-    cvtColor(detectedA4, gray, COLOR_BGR2GRAY);
+    cvtColor(image, gray, COLOR_BGR2GRAY);
 
-    // Kenarları tespit etme
+    // Kenarları algıla (GaussianBlur ve Canny kenar dedektörü kullanıldı)
+    Mat blurred;
+    GaussianBlur(gray, blurred, Size(5, 5), 0);
     Mat edges;
-    Canny(gray, edges, 50, 150, 3);
+    Canny(blurred, edges, 50, 150);
 
-    // Contourları bulma
+    // Kenarları iyileştirme (dilate ve erode)
+    Mat dilated;
+    dilate(edges, dilated, Mat(), Point(-1, -1), 2);
+    Mat eroded;
+    erode(dilated, eroded, Mat(), Point(-1, -1), 1);
+
+    // Konturları bul
     std::vector<std::vector<Point>> contours;
-    findContours(edges, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-    //Kare ve daire vektörleri oluşturma
-    std::vector<std::vector<Point>> squares;
-    std::vector<std::vector<Point>> circles;
+    findContours(eroded, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
+    // Metin ve kutu vektörleri
+    std::vector<Rect> textRectangles;
+    std::vector<Rect> boxRectangles;
+    std::vector<Rect> choiceRectangles;
+
+    // Her konturu değerlendir
     for (size_t i = 0; i < contours.size(); i++) {
-        // Konturun alanını hesaplama
-        double area = contourArea(contours[i]);
+        Rect boundingRect = cv::boundingRect(contours[i]);
 
-        // Kare ya da daire olup olmadığını kontrol etme
-        if (area > 500) { // Eşik değeri, algılanacak şeklin alanına bağlı olarak değişebilir
-            std::vector<Point> approx;
-            approxPolyDP(contours[i], approx, arcLength(contours[i], true) * 0.02, true);
+        // Belirli bir boyut aralığına sahip dikdörtgenleri kabul et (checkbox'ları filtrele)
+        int minCheckboxSize = 10;
+        int maxCheckboxSize = 22;
 
-            if (approx.size() == 4) {
-                // Kare
-                squares.push_back(approx);
-            } else {
-                // Daire
-                Point2f center;
-                float radius;
-                minEnclosingCircle(contours[i], center, radius);
-                if (std::abs(area - CV_PI * radius * radius) < 100) {
-                    circles.push_back(contours[i]);
-                }
-            }
+        if (boundingRect.width > minCheckboxSize && boundingRect.width < maxCheckboxSize &&
+            boundingRect.height > minCheckboxSize && boundingRect.height < maxCheckboxSize) {
+            // Checkbox ise yeşil ile çiz
+            rectangle(image, boundingRect, Scalar(0, 255, 0), 2);
+            boxRectangles.push_back(boundingRect);
+        } else {
+            // Diğer konturları mavi ile çiz
+            rectangle(image, boundingRect, Scalar(255, 0, 0), 2);
+
+            // Mavi çevrelenen kontürler kutu olarak kabul edilsin
+            textRectangles.push_back(boundingRect);
         }
     }
 
-    // Kare ve daireleri fotoğraf üzerinde çizme
-    // Kırmızı renkli ve 2 piksel kalınlığında bir kalem oluşturma
-    Scalar color = Scalar(0, 0, 255);
-    int thickness = 2;
+    // Metin ve kutu vektörlerini sırala
+    std::sort(textRectangles.begin(), textRectangles.end(), [](const Rect &a, const Rect &b) {
+        return a.y < b.y;
+    });
 
-    // Kare vektöründeki her bir eleman için
-    for (size_t i = 0; i < squares.size(); ++i) {
-        // Fotoğraf üzerinde kare çizme
-        polylines(detectedA4, squares[i], true, color, thickness);
+    std::sort(boxRectangles.begin(), boxRectangles.end(), [](const Rect &a, const Rect &b) {
+        return a.y < b.y;
+    });
+
+/// Kontrol et ve etiketle
+    for (size_t i = 0; i < boxRectangles.size(); ++i) {
+        rectangle(image, boxRectangles[i], Scalar(255, 0, 0), 2);
+
+        // Kutu numarasını ekle
+     //   putText(image, "Kutu-" + std::to_string(i + 1), Point(boxRectangles[i].x + boxRectangles[i].width + 5, boxRectangles[i].y + 10),
+        //        FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 1);
+
+        // "choice" olarak işaretlenmiş mi?
+        bool isChoice = false;
+
+        // Text rectangles ile kontrol et
+        bool isCheckbox = true;  
+        bool isQuestion = false;
+        for (size_t j = 0; j < textRectangles.size(); ++j) 
+        {
+            if (abs(boxRectangles[i].y - textRectangles[j].y) < 5) 
+            {
+                // Eğer aynı yükseklikte ise "choice"
+                rectangle(image, textRectangles[j], Scalar(0, 255, 0), 2);
+                //putText(image, "choice", Point(textRectangles[j].x + textRectangles[j].width + 5, textRectangles[j].y + 10),
+                      //  FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 1);
+                isChoice = true;
+                isQuestion = false;  // Eğer bir seçenek bulunduysa, bu kutu bir soru değildir
+                // Checkbox içerisi dolu mu kontrol et
+            Mat roi = gray(boxRectangles[i]);  // Checkbox'ın olduğu bölgeyi al (Gri tonlamalı resmi kullan)
+           
+            // İkili görüntüye çevir
+            threshold(roi, roi, 128, 255, THRESH_BINARY);
+
+            // Beyaz piksel sayısını kontrol et
+            int whitePixelCount = countNonZero(roi);
+
+            // Eğer beyaz piksel sayısı belirli bir eşik değerinden büyükse "checked" yazısını ekle
+                if (whitePixelCount < (roi.rows * roi.cols) * 0.5) {
+                    putText(image, "X", Point(boxRectangles[i].x + boxRectangles[i].width + 5, boxRectangles[i].y + 10),
+                            FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255), 1);
+                            bool isAnswer = true;
+            }
+            }
+             else {
+
+                rectangle(image, boxRectangles[i], Scalar(0, 0, 255), 2);  // Kırmızı çerçeve ekle
+                
+            }
+            // Döngüden çıkmak için ESC tuşuna basılıp basılmadığını kontrol edin
+       if (waitKey(1) == 27) {
+        break;
+       }
+            
+        }
     }
     
 
-    // Daire vektöründeki her bir eleman için
-    for (size_t i = 0; i < circles.size(); ++i) {
-        // Fotoğraf üzerinde daire çizme
-        Point2f center;
-        float radius;
-        minEnclosingCircle(circles[i], center, radius);
-        circle(detectedA4, center, radius, color, thickness);
-    }
-
-    // Fotoğrafı ekrana gösterme
-    imshow("Detected Shapes", detectedA4);
+    // Sonucu göster
+    imshow("Sıralı Checkbox ve Metin Tespiti", image);
     waitKey(0);
 
     return 0;
